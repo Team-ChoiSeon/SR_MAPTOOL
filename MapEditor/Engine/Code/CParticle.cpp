ccda60faf071e0662d46dea694aa484850900a68
boogiu <boogiww@gmail.com>
2025-07-12 00:41:30 +0900

    feat : particle set #40

--- MapEditor/Engine/Code/CParticle.cpp
+++ MapEditor/Engine/Code/CParticle.cpp
@@ -34,6 +34,10 @@ void CParticle::LateUpdate_Component(_float& dt)
 {
 }
 
+void CParticle::Render_Particle(LPDIRECT3DDEVICE9 pDevice)
+{
+}
+
 void CParticle::Render_Panel(ImVec2 size)
 {
 }
@@ -45,10 +49,28 @@ CComponent* CParticle::Clone() const
 
 void CParticle::Serialize(json& outJson) const
 {
+	outJson["effect"] = m_strEffectName;
+	outJson["loop"] = m_bLoop;
+	outJson["duration"] = m_fDuration;
+	outJson["spawn_rate"] = m_fSpawnRate;
+	outJson["life_time"] = m_fLifeTime;
+	outJson["start_Scale"] = { m_vStartScale.x,m_vStartScale.y,m_vStartScale.z };
+	outJson["end_scale"] = { m_vEndScale.x, m_vEndScale.y, m_vEndScale.z };
 }
 
 void CParticle::Deserialize(const json& inJson)
 {
+	m_strEffectName = inJson.value("effect", "Fire");
+	m_bLoop = inJson.value("loop", true);
+	m_fDuration = inJson.value("duration", 3.f);
+	m_fSpawnRate = inJson.value("spawn_rate", 10.f);
+	m_fLifeTime = inJson.value("life_time", 1.f);
+
+	auto s = inJson["start_scale"];
+	m_vStartScale = { s[0], s[1], s[2] };
+
+	auto e = inJson["end_scale"];
+	m_vEndScale = { e[0], e[1], e[2] };
 }
 
 string CParticle::Get_ComponentName() const
--- MapEditor/Engine/Header/CParticle.h
+++ MapEditor/Engine/Header/CParticle.h
@@ -12,13 +12,26 @@ public:
     HRESULT Ready_Component() override;
     void Update_Component(_float& dt) override;
     void LateUpdate_Component(_float& dt) override;
+    void Render_Particle(LPDIRECT3DDEVICE9 pDevice);
     void Render_Panel(ImVec2 size) override;
+
     CComponent* Clone() const override;
     void Serialize(json& outJson) const override;
     void Deserialize(const json& inJson) override;
     string Get_ComponentName() const override;
 private:
-    //float m_Lig
+    string m_strEffectName;
+    _bool m_bLoop;
+    _float m_fDuration;
+    _float m_fSpawnRate;
+    _float m_fLifeTime;
+    _vec3 m_vStartColor;//시작 색상
+    _vec3 m_vEndColor;//종료 색상
+
+    _vec3 m_vStartScale; //시작 크기
+    _vec3 m_vEndScale; // 종료 크기
+
+
 private:
     void Free() override;
 };
